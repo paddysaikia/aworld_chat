ea208fb9194a04249bcff8bfd10367319438f83f
paddysaikia <paddysaikia+git@gmail.com>
2024-12-09 00:31:21 +0100

    prod start

--- chat_gpt.py
+++ chat_gpt.py
@@ -26,7 +26,8 @@ def chat():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from Render or fallback to 5000
+    app.run(host='0.0.0.0', port=port)
 
 
 
